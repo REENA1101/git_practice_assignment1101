45358fcfb95e356d2587e14df8308877a2787d49
Reena kumari <reenakvs1@gmail.com>
2022-11-03 12:40:24 +0530

    created palindrome and prime code

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+let str="abccba"
+let new_str=""
+
+for(let i=str.length-1;i>=0;i++){
+  new_str+=str[i];
+  if(new_str==str){
+    print="Yes"
+  }else{
+    print="No"
+  }
+  }
+console.log(printssss);
+
